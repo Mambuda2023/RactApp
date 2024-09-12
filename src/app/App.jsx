f4efadd2350b69eaf59c7432ec23ed0bbf722888
Мазманян Юра <mamzmanyan2018@list.ru>
2024-09-12 22:22:03 +0300

    Создание компонента JournalItem и CardButton

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,27 @@
 import { Button } from "../shared/UI/Button/Button";
+import CardButton from "../widgets/CardButton/CardButton";
+import JournalItem from "../widgets/JournalItem/JournalItem";
 import "./App.css";
 
 function App() {
+  const data = [
+    {
+      title: "Подговка к обновлению курсов",
+      text: "Горные походы открывают удивительные природные ландшафты",
+      date: new Date(),
+    },
+  ];
   return (
     <>
       <Button />
+
+      <CardButton>
+        <JournalItem
+          title={data[0].title}
+          text={data[0].text}
+          date={data[0].date}
+        />
+      </CardButton>
     </>
   );
 }
---
+++ src/widgets/JournalItem/JournalItem.jsx
@@ -0,0 +1,18 @@
+import "./JournalItem.css";
+const JournalItem = ({ title, text, date }) => {
+  const formatDate = new Intl.DateTimeFormat('ru-RU').format(date)
+  return (
+    <>
+      <h2 className="journal-item__title">{title}</h2>
+      <div className="journal-item__body">
+        <footer>
+          <data className="journal-item__date" dateTime="2005-10-05">
+            {formatDate}
+          </data>
+        </footer>
+        <p className="journal-item__text">{text}</p>
+      </div>
+    </>
+  );
+};
+export default JournalItem;
